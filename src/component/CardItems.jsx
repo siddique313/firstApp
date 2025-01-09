1168e0a8d86dc76311e1aafc800da6e897c14938
muhammad ali <alymohd487@gmail.com>
2025-01-09 17:14:31 +0500

    wip

--- src/component/CardItems.jsx
+++ src/component/CardItems.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React from "react";
 
-export default function CardItems({item}) {
-  return (<div  className='flex flex-col shadow-2xl'>
-                <div className=' flex flex-col items-center justify-center text-center'>
-                    <h1 className='text-center px-3 font-bold font-serif py-5'>{item.title.slice(0,15)} </h1>
-                    <img className='object-contain h-52 px-5 max-w-xs transition duration-300 ease-in-out hover:scale-110' src={item.image} />
-                </div>
-                <div className='px-6 py-3 flex flex-col text-start gap-3'>
-                <h1 className='text-center'>{item.category} </h1>
-                    <p className={`text-center text-white bg-blue-950`}>{item.rating.rate }</p>
-                    <p className=''>{item.description.slice(0,70)} </p>
+export default function CardItems({ item }) {
+  return (
+    <div className="flex flex-col shadow-2xl">
+      <div className=" flex flex-col items-center justify-center text-center">
+        <h1 className="text-center px-3 font-bold font-serif py-5">
+          {item.title.slice(0, 15)}{" "}
+        </h1>
+        <img
+          className="object-contain h-52 px-5 max-w-xs transition duration-300 ease-in-out hover:scale-110"
+          src={item.image}
+        />
+      </div>
+      <div className="px-6 py-3 flex flex-col text-start gap-3">
+        <h1 className="text-center">{item.category} </h1>
+        <p className={`text-center text-white bg-blue-950`}>
+          {item.rating.rate}
+        </p>
+        <p className="">{item.description.slice(0, 70)} </p>
 
-                    {/* <span>{item.price}</span> */}
-                </div>
-            </div>  
-           
-      
-  )
+        {/* <span>{item.price}</span> */}
+      </div>
+    </div>
+  );
 }
--- src/component/card.jsx
+++ src/component/card.jsx
@@ -1,49 +1,60 @@
-import React, { useEffect, useState } from 'react'
-import CardItems from './CardItems';
-import axios from 'axios';
-
+import React, { useEffect, useState } from "react";
+import CardItems from "./CardItems";
+import axios from "axios";
 
 export default function Card() {
-    const [testApi, setTestApi] = useState([])
-    const api = "https://fakestoreapi.com/products"
-    const fetchApi = async () => {
-        await axios.get(api).then(res=>{
-            setTestApi(res.data)
-        })
-    }
-  useEffect(()=>{
-fetchApi()
-  },[])
-if(testApi <= 0){
-    return <div className='w-screen h-screen flex items-center justify-center bg-slate-600 '>
-      <div
-    className=" h-8 w-8  animate-spin rounded-full border-4 border-solid border-white border-current border-r-transparent flex align-center items-center justify-center motion-reduce:animate-[spin_1.5s_linear_infinite]"
-    role="status">
-    <span
-      className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-    >Loading...</span>
-  </div>
-    </div>
-}
-  return  <>
-  <div className='w-full flex items-center justify-center  py-8'>
-    <input className='outline-none border-2 border-gray-200 pl-2 pr-9 rounded-lg' type="text" placeholder='How you need' />
-  </div>
-  <div className='w-full  flex gap-4 items-center justify-center'>
-    <button className='bg-slate-200 px-5 py-2 rounded-xl '>All Catagory</button>
-    <button className='bg-slate-200 px-5 py-2 rounded-xl '>men's clothing</button>
-    <button className='bg-slate-200 px-5 py-2 rounded-xl '>women's clothing</button>
-    <button className='bg-slate-200 px-5 py-2 rounded-xl '>electronics</button>
-  </div>
-  <div className='grid grid-cols-4 gap-3 px-8'>
-      {
-        testApi.map((item,index)=>{
-            return <CardItems key={index} item={item}/> 
-           
-        })
-      }
-    </div>
-  </>
-  
-
+  const [testApi, setTestApi] = useState([]);
+  const api = "https://fakestoreapi.com/products";
+  const fetchApi = async () => {
+    await axios.get(api).then((res) => {
+      setTestApi(res.data);
+    });
+  };
+  useEffect(() => {
+    fetchApi();
+  }, []);
+  if (testApi <= 0) {
+    return (
+      <div className="w-screen h-screen flex items-center justify-center bg-slate-600 ">
+        <div
+          className=" h-8 w-8  animate-spin rounded-full border-4 border-solid border-white border-current border-r-transparent flex align-center items-center justify-center motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status"
+        >
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <>
+      <div className="w-full flex items-center justify-center  py-8">
+        <input
+          className="outline-none border-2 border-gray-200 pl-2 pr-9 rounded-lg"
+          type="text"
+          placeholder="How you need"
+        />
+      </div>
+      <div className="w-full  flex gap-4 items-center justify-center">
+        <button className="bg-slate-200 px-5 py-2 rounded-xl ">
+          All Catagory
+        </button>
+        <button className="bg-slate-200 px-5 py-2 rounded-xl ">
+          men's clothing
+        </button>
+        <button className="bg-slate-200 px-5 py-2 rounded-xl ">
+          women's clothing
+        </button>
+        <button className="bg-slate-200 px-5 py-2 rounded-xl ">
+          electronics
+        </button>
+      </div>
+      <div className="grid grid-cols-4 gap-3 px-8">
+        {testApi.map((item, index) => {
+          return <CardItems key={index} item={item} />;
+        })}
+      </div>
+    </>
+  );
 }
